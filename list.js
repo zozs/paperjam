7c08a2cb306c37b686d649f79170dab5f6b45a76
Linus Karlsson <linus@zozs.se>
2015-10-16 20:08:34 +0200

    Add pagination for list page.
    Fixes #6.

--- list.js
+++ list.js
@@ -3,9 +3,14 @@
  * See the file LICENSE at https://github.com/zozs/paperjam
  */
 
-paperjamApp.controller('ListDocumentsCtrl', function ($http) {
+paperjamApp.controller('ListDocumentsCtrl', function ($http, urls) {
   var self = this;
+  this.urls = urls;
+
   $http.get('api/documents').success(function (data) {
     self.documents = data.documents;
   });
+
+  this.documentsCurrentPage = 1;
+  this.documentsPerPage = 20;
 });
